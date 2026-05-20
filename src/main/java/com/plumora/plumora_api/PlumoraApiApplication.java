49a7521bb9139d2546a272ab6091591219833a1f
Fonkou kevin <117923304+fonkoukevin@users.noreply.github.com>
2026-05-20 22:59:56 +0200

    set up docker

---
+++ src/main/java/com/plumora/plumora_api/PlumoraApiApplication.java
@@ -0,0 +1,13 @@
+package com.plumora.plumora_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlumoraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlumoraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plumora/plumora_api/PlumoraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plumora.plumora_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlumoraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
